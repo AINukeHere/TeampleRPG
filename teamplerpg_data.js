8aed0bcd8b8b91d886e37bf41cb38855b8de0a09
AINukeHere <iii4625@naver.com>
2021-07-14 23:49:27 +0900

    hidden1 update

--- teamplerpg_data.js
+++ teamplerpg_data.js
@@ -1,68 +1,23 @@
-var request = new XMLHttpRequest();
-request.open("GET", "https://raw.githubusercontent.com/AINukeHere/TeampleRPG/main/data/class_info.json", false);
-request.send(null)
-var jsonObject = JSON.parse(request.responseText);
-function setInfoPosition(event)
+var _DEBUG = true;
+var jsonObject = jsonObject[0];
+if(!_DEBUG)
 {
-    var classInfo = document.getElementById("classInfo");
-    classInfo.style.left=(event.pageX+5).toString()+"px";
-    classInfo.style.top=(event.pageY+5).toString()+"px";
-    //console.log(event.clientX, event.clientY);
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://raw.githubusercontent.com/AINukeHere/TeampleRPG/main/data/class_info.json", false);
+    request.send(null)
+    jsonObject = JSON.parse(request.responseText);
 }
-function showClassInfo(classIdx)
+function setPopupInfoPosition(event)
 {
-    var classInfo = document.getElementById("classInfo");
-    classInfo.style.display="inline";
-    classInfo.children[0].innerHTML = jsonObject.classes[classIdx].explanation;
-    classInfo.children[1].src = 'data/'+jsonObject.classes[classIdx].image;
-}
-function onClickClass(classIdx)
-{
-    toggleclassTable();
-    var classView = document.getElementById("classView");
-    var innerHTML_str = "\
-    <img class='classProfileImage' src=data/"+jsonObject.classes[classIdx].image+"'>\
-    <span>"+jsonObject.classes[classIdx].explanation+"</span>\
-    <br>\
-    <table border='1' style='border:2px double red'>\
-    ";
-    var classInfo = jsonObject.classes[classIdx];
-    for(var i = 0; i < classInfo.skills.length; ++i)
-    {
-        var skillInfo = classInfo.skills[i];
-        innerHTML_str += "\
-        <tr>\
-            <td align='center' colspan='2'>";
-            for(var cmdIdx = 0; skillInfo.command.length; ++cmdIdx)
-            {
-                innerHTML_str += "\
-                <img src='data/"+skillInfo.command[cmdIdx]+"_Skill.png'>";
-                console.log(jsonObject.classes[classIdx].skills[i].command[cmdIdx]);
-            }
-            innerHTML_str += "\
-            </td>\
-        </tr>\
-        <tr>\
-            <td>\
-                <span>"+skillInfo.name+"</span>\
-            </td>\
-            <td>\
-                <span>"+skillInfo.explanation+"</span>\
-            </td>\
-        </tr>\
-        ";
-    }
-    innerHTML_str+="\
-    </table>\
-    ";
-    classView.innerHTML= innerHTML_str;
+    var classPopupInfo = document.getElementById("classPopupInfo");
+    classPopupInfo.style.left=(event.pageX+5).toString()+"px";
+    classPopupInfo.style.top=(event.pageY+5).toString()+"px";
 
-    // var classProfileImage = classView.getElementsByClassName("classProfileImage");
-    // classProfileImage[0].src = 'data/' + jsonObject.classes[classIdx].image;
-    // var Skill1_explanation = document.getElementById("Skill1_explanation");
-    // Skill1_explanation.innerHTML=jsonObject.classes[classIdx].Skill1_explanation;
-    // var Skill1_name = document.getElementById("Skill1_name");
-    // Skill1_name.innerHTML=jsonObject.classes[classIdx].Skill1_name;
+    var skillPopupInfo = document.getElementById("skillPopupInfo");
+    skillPopupInfo.style.left=(event.pageX+5).toString()+"px";
+    skillPopupInfo.style.top=(event.pageY+5).toString()+"px";
+    
+    //console.log(event.clientX, event.clientY);
 }
 function toggleclassTable()
 {
@@ -72,8 +27,91 @@ function toggleclassTable()
     else
         classTable.style.display = "none";
 }
-function hideClassInfo(obj)
+function showClassPopupInfo(classIdx)
 {
-    var classInfo = document.getElementById("classInfo");
-    classInfo.style.display="none";
+    var classPopupInfo = document.getElementById("classPopupInfo");
+    classPopupInfo.style.display="inline";
+    classPopupInfo.children[0].innerHTML = jsonObject.classes[classIdx].explanation;
+    classPopupInfo.children[1].src = 'data/'+jsonObject.classes[classIdx].image;
+}
+function hideClassPopupInfo(obj)
+{
+    var classPopupInfo = document.getElementById("classPopupInfo");
+    classPopupInfo.style.display="none";
+}
+function showSkillPopupInfo(skillCommand)
+{
+    var skillPopupInfo = document.getElementById("skillPopupInfo");
+    skillPopupInfo.style.display="inline";
+    if(skillCommand == "P")
+        skillPopupInfo.getElementsByTagName("img")[0].s
+    skillPopupInfo.getElementsByTagName("img")[0].src = "data/"+skillCommand+"_Skill_Cost.png";
+}
+function hideSkillPopupInfo()
+{
+    var skillPopupInfo = document.getElementById("skillPopupInfo");
+    skillPopupInfo.style.display="none";
+}
+
+function onClickClass(classIdx)
+{
+    toggleclassTable();
+    var classInfo = jsonObject.classes[classIdx];
+    var classView = document.getElementById("classView");
+    
+    var spec_str1="";
+    var spec_str2="";
+    var spec_str3="";
+    for(var i = 5, spec1 = classInfo.spec1,spec2 = classInfo.spec2, spec3 = classInfo.spec3; i > 0; --i,--spec1, --spec2, --spec3){
+        spec_str1 += (spec1 > 0) ? "★" : "☆";
+        spec_str2 += (spec2 > 0) ? "★" : "☆";
+        spec_str3 += (spec3 > 0) ? "★" : "☆";
+    }
+
+    var innerHTML_str = "\
+    <img class='classProfileImage' src=data/"+jsonObject.classes[classIdx].image+"><br>\
+    <span class='specName'>파괴력</span><span class='specStars'>"+spec_str1+"</span><br>\
+    <span class='specName'>내구력</span><span class='specStars'>"+spec_str2+"</span><br>\
+    <span class='specName'>기동성</span><span class='specStars'>"+spec_str3+"</span><br>\
+    <span class='explanation'>"+jsonObject.classes[classIdx].explanation+"</span>\
+    <br>\
+    <div class='skillViewer'>\
+    ";
+    for(var i = 0; i < classInfo.skills.length; ++i)
+    {
+        var skillInfo = classInfo.skills[i];
+        innerHTML_str += "\
+        <table class='skillContent'>\
+        <tr>\
+            <td>";
+            for(var cmdIdx = 0; cmdIdx < skillInfo.command.length; ++cmdIdx)
+            {
+                innerHTML_str += "\
+                <img class='skillIcon'\
+                onmousemove='showSkillPopupInfo(\""+skillInfo.command[cmdIdx]+"\")' \
+                onmouseout='hideSkillPopupInfo()' \
+                src='data/"+skillInfo.command[cmdIdx]+"_Skill.png'>";
+                console.log(jsonObject.classes[classIdx].skills[i].command[cmdIdx]);
+            }
+            innerHTML_str += "\
+            </td>\
+        </tr>\
+        <tr>\
+            <td>\
+                <span class='skillCommand'>"+skillInfo.command+" Skill - </span>\
+                <span class='skillName'>"+skillInfo.name+"</span>\
+            </td>\
+        </tr>\
+        <tr>\
+            <td>\
+                <span class='explanation'>"+skillInfo.explanation+"</span>\
+            </td>\
+        </tr>\
+        </table>\
+        ";
+    }
+    innerHTML_str+="\
+    </div>\
+    ";
+    classView.innerHTML= innerHTML_str;
 }
