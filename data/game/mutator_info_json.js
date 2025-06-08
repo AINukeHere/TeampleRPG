70b57471285820503807e52d5e76ba126aac640d
AINukeHere <iii4625@naver.com>
2025-06-09 04:57:49 +0900

    2025.06.09T

--- data/game/mutator_info_json.js
+++ data/game/mutator_info_json.js
@@ -1,66 +1,118 @@
+// 밝혀지지않았거나 미구현된 돌연변이원에 대해서 스포일러가 포함되어있을 수 있습니다.
 mutatorData=[
     {
+        "status":"disabled",
         "name":"파업",
         "explanation":"몇몇 캐릭터를 선택할 수 없게됩니다."
     },
     {
+        "status":"enabled",
         "name":"원코인",
         "explanation":"스테이지가 끝나고 죽은 플레이어중 단 한명만 부활합니다."
     },
     {
+        "status":"enabled",
         "name":"재생",
         "explanation":"지속적으로 적 병력 체력이 재생됩니다.(보스제외, 인원수비례)"
     },
     {
+        "status":"disabled",
         "name":"업보",
         "explanation":"가장 킬수가 높은 플레이어의 가스재생량이 4 감소힙니다."
     },
     {
+        "status":"enabled",
         "name":"신경삭을 끊어라!",
         "explanation":"자신의 시야만 남고 나머지는 사라집니다."
     },
     {
+        "status":"enabled",
         "name":"기억상실증",
         "explanation":"보스가 때때로 스킬을 사용한것을 잊어버리고 스킬을  한번 더 씁니다."
     },
     {
+        "status":"enabled",
         "name":"완판",
         "explanation":"상점에서 판매히는 포션의 재고가 플레이어수 *2개로 제한됩니다."
     },
     {
+        "status":"enabled",
         "name":"바이러스",
         "explanation":"적 병력들이 마음대로 움직이며 몇몇 유닛들이 자신을 복제하여 소환합니다."
     },
     {
+        "status":"enabled",
         "name":"해치웠나",
         "explanation":"처치했다고 생각한 보스가 다음 스테이지에서 다시 나타납니다."
     },
     {
+        "status":"enabled",
         "name":"속전속결",
         "explanation":"상점시간이 45초로 제한됩니다. 필드를 모두 정리하면 바로 보스존으로 진입합니다."
     },
     {
+        "status":"enabled",
         "name":"특급배달",
         "explanation":"게임이 끝날때까지 주기적으로 플레이어들의 위치에 적 유닛들이 소환됩니다."
     },
     {
+        "status":"disabled",
         "name":"오버플로우",
         "explanation":"한 플레이어의 마나량이 최대치를 넘어서면 모든 플레이어가 마나번 디버프에 걸립니다."
     },
     {
+        "status":"disabled",
         "name":"사회적 거리두기",
         "explanation":"각 플레이어의 주 캐릭터 기준 10x10 내에 지상동맹유닛이 3초이상 머무르면 해당 플레이어가 스킬을 사용할 수 없게 됩니다."
     },
     {
+        "status":"enabled",
         "name":"복병",
         "explanation":"몇몇 적 병력들이 스킬을 사용합니다."
     },
     {
+        "status":"enabled",
         "name":"방전",
         "explanation":"플레이어의 모든 유닛의 쉴드와 마나가 0으로 고정됩니다."
     },
     {
+        "status":"disabled",
         "name":"배신",
         "explanation":"1분마다 10초동안 모든 팀원과 적대관계가 됩니다."
+    },
+    {
+        "status":"enabled",
+        "hidden":true,
+        "name":"방어막",
+        "explanation":"모든 적 유닛이 피해를 받으면 250의 방어막을 얻습니다. 스테이지를 클리어할수록 보호횟수가 증가합니다.",
+        "hidden_name_hint":"ㅂㅇㅁ"
+    },
+    {
+        "status":"disabled",
+        "name":"좀 죽어!",
+        "explanation":"보스를 제외한 모든 적 유닛이 자동으로 1회 부활합니다.",
+        "hidden":true,
+        "hidden_name_hint":"ㅈㅈㅇ!"
+    },
+    {
+        "status":"disabled",
+        "name":"아무도 모르게",
+        "explanation":"모든 적 유닛이 영구적으로 은신 상태가 됩니다.",
+        "hidden":true,
+        "hidden_name_hint":"ㅇㅁㄷㅁㄹㄱ"
+    },
+    {
+        "status":"enabled",
+        "name":"업그레이드가 쵝오!",
+        "explanation":"시간이 지남에 따라 적이 업그레이드를 진행합니다.",
+        "hidden":true,
+        "hidden_name_hint":"ㅇㄱㄹㅇㄷㄱㅊㅇ!"
+    },
+    {
+        "status":"disabled",
+        "name":"에일리언 부화",
+        "explanation":"적 유닛이 죽으면 체력에 비례하여 맹독충이 생성됩니다.",
+        "hidden":true,
+        "hidden_name_hint":"ㅇㅇㄹㅇㅂㅎ"
     },
 ];
--- mutator_info_section.js
+++ mutator_info_section.js
@@ -2,12 +2,33 @@ var mutatorInfoDiv = document.getElementById("MutatorInfomation").getElementsByT
 
 var innerHTML_str = "";
 
-for(var i =0; i < mutatorData.length; ++i)
+for(var i =0; i < mutatorData.length; ++i){
+
     innerHTML_str += "\
-    <div>\
-    <span class='DPTEXT_07'>" + mutatorData[i].name + "</span><br>\
-    <span class='DPTEXT_04'>" + mutatorData[i].explanation + "</span>\
-    </div>\
-    ";
+    <div>";
+    if (mutatorData[i].hidden == true){
+        innerHTML_str += "\
+        <span class='DPTEXT_07'>" + mutatorData[i].hidden_name_hint + "</span>";
+        if (mutatorData[i].status === "disabled")
+            innerHTML_str += "<span class='DPTEXT_08'>(자동선택으로 선택되지 않음)</span><br>";
+        else
+            innerHTML_str += "<br>";
+        innerHTML_str += "<span class='DPTEXT_04'>[아직 밝혀진 바 없음]</span>\
+        </div>\
+        ";
+    }
+    else{
+        innerHTML_str += "<span class='DPTEXT_07'>" + mutatorData[i].name + "</span>";
+        if (mutatorData[i].status === "disabled")
+            innerHTML_str += "<span class='DPTEXT_08'>(자동선택으로 선택되지 않음)</span><br>";
+        else{
+            innerHTML_str += "<br>";
+        }
+        innerHTML_str += "<span class='DPTEXT_04'>" + mutatorData[i].explanation + "</span>\
+        </div>\
+        ";
+    }
+}
+
 
 mutatorInfoDiv.innerHTML = innerHTML_str;
